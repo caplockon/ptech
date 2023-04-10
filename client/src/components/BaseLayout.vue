5780cbdaffe1f0ca3249684d447c5625c7df5265
Phuc Nguyen <phucnguyen@besmartee.com>
2023-04-10 10:46:37 +0700

    Setup app components

---
+++ client/src/components/BaseLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <slot name="starting"></slot>
+    <header>
+        <!-- We want header content here -->
+    </header>
+    <main>
+        <slot></slot>
+    </main>
+    <footer>
+        <!-- We want footer content here -->
+    </footer>
+    <slot name="ending"></slot>
+</template>
+
+<script>
+export default {
+    name: "BaseLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
